bd7dc56305337df4b684db3b01215addb79f09a8
dinghaiyang <ding-haiyang@qq.com>
2024-04-25 10:18:43 +0800

    删除脚本

--- snippets/startuprun.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-set "EXE_PATH=%~dp0\snippets.exe"
-reg add HKLM\Software\Microsoft\Windows\CurrentVersion\Run /v snippets /t REG_SZ /d "%EXE_PATH%" /f
-echo %EXE_PATH% set to run at startup.
